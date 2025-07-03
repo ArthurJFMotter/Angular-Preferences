1d25b69ae6d619df68f25064efcb3b500ab46b9d
ArthurJFMotter <arthur.jf.motter@gmail.com>
2025-07-03 15:41:40 -0300

    test

--- angular-preferences-app/src/app/app.component.ts
+++ angular-preferences-app/src/app/app.component.ts
@@ -1,20 +1,46 @@
 import { Component, inject } from '@angular/core';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatIconModule } from '@angular/material/icon';
+import { FormsModule } from '@angular/forms';
+/* will be moved to an module */
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { ThemeService, Theme } from './services/theme.service';
+/* will be moved to an module */
 
 @Component({
   selector: 'app-root',
+  /* will be moved to an module */
   imports: [
-    MatToolbarModule,
-    MatIconModule,
+    FormsModule,
     MatButtonModule,
     MatCardModule,
+    MatCheckboxModule,
+    MatChipsModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
     MatMenuModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule,
+    MatSelectModule,
+    MatSliderModule,
+    MatSlideToggleModule,
+    MatTabsModule,
+    MatToolbarModule,
   ],
+  /* will be moved to an module */
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
