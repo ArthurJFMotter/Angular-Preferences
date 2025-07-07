c710e5b7d2f0e9f1a58f1a22e2016a9d5d214791
ArthurJFMotter <arthur.jf.motter@gmail.com>
2025-07-07 09:45:39 -0300

    better helper

--- angular-preferences-app/src/app/components/preferences-helper/preferences-helper.component.ts
+++ angular-preferences-app/src/app/components/preferences-helper/preferences-helper.component.ts
@@ -3,17 +3,24 @@ import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
+import { MatTooltipModule } from '@angular/material/tooltip';
 import { ThemeService } from '../../services/theme.service';
 import { PreferencesService } from '../../services/preferences.service';
 
 @Component({
   selector: 'app-preferences-helper',
   standalone: true,
-  imports: [CommonModule, MatButtonModule, MatIconModule, MatMenuModule],
+  imports: [CommonModule, MatButtonModule, MatIconModule, MatMenuModule, MatTooltipModule],
   templateUrl: './preferences-helper.component.html',
   styleUrls: ['./preferences-helper.component.scss']
 })
 export class PreferencesHelperComponent {
   themeService = inject(ThemeService);
   preferencesService = inject(PreferencesService);
+
+  isExpanded = false;
+
+  toggleExpansion(): void {
+    this.isExpanded = !this.isExpanded;
+  }
 }
