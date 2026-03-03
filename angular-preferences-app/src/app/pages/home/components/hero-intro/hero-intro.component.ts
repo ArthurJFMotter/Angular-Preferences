fc160957ab63f9f44e66152973f5b045b2b9b93f
ArthurJFMotter <arthur.jf.motter@gmail.com>
2026-03-03 12:04:40 -0300

    feat(home):shape & flow fix

--- angular-preferences-app/src/app/pages/home/components/hero-intro/hero-intro.component.ts
+++ angular-preferences-app/src/app/pages/home/components/hero-intro/hero-intro.component.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { SnackbarService } from '../../../../services/snackbar.service';
 
 @Component({
   selector: 'app-hero-intro',
@@ -13,8 +14,14 @@ import { MatIconModule } from '@angular/material/icon';
 })
 export class HeroIntroComponent {
   private router = inject(Router);
+  snackbarService = inject(SnackbarService);
 
   navigateToSettings() {
     this.router.navigate(['/configurations']);
+  }
+
+  // temporary function, remove when documentation is ready
+  handleDocumentation() {
+    this.snackbarService.info('Documentation is on the works, and will be added soon.');
   }
 }
