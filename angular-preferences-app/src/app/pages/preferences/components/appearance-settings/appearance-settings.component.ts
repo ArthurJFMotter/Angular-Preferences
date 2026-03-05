d6206647f6d7508d41fd491bcc624ea8f70eba83
ArthurJFMotter <arthur.jf.motter@gmail.com>
2026-03-05 13:48:03 -0300

    feat(preferences): rounding changes

--- angular-preferences-app/src/app/pages/preferences/components/appearance-settings/appearance-settings.component.ts
+++ angular-preferences-app/src/app/pages/preferences/components/appearance-settings/appearance-settings.component.ts
@@ -1,16 +1,17 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal, effect } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatTooltipModule } from '@angular/material/tooltip';
-import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatSliderModule } from '@angular/material/slider';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 import { ThemeService } from '../../../../services/theme.service';
 import { PreferencesService } from '../../../../services/preferences.service';
 import { ShapeService } from '../../../../services/shape.service';
 import { HelpButtonComponent } from '../../../../components/help-button/help-button.component';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
 
 @Component({
   selector: 'app-appearance-settings',
@@ -18,11 +19,13 @@ import { HelpButtonComponent } from '../../../../components/help-button/help-but
   imports: [
     CommonModule,
     HelpButtonComponent,
+    MatButtonToggleModule,
     MatCardModule,
     MatIconModule,
     MatButtonModule,
     MatTooltipModule,
-    MatButtonToggleModule,
+    MatSliderModule,
+    MatSlideToggleModule,
   ],
   templateUrl: './appearance-settings.component.html',
   styleUrls: ['./appearance-settings.component.scss'],
@@ -31,4 +34,30 @@ export class AppearanceSettingsComponent {
   themeService = inject(ThemeService);
   preferencesService = inject(PreferencesService);
   shapeService = inject(ShapeService);
+
+  readonly shapes = this.shapeService.getShapes();
+  activeShape = this.shapeService.activeShape;
+
+  shapeSliderIndex = signal(
+    this.shapes.findIndex((s) => s.id === this.activeShape().id)
+  );
+
+  constructor() {
+    // Sync slider when shape changes externally
+    effect(() => {
+      const currentId = this.activeShape().id;
+      const newIndex = this.shapes.findIndex((s) => s.id === currentId);
+      if (newIndex > -1 && newIndex !== this.shapeSliderIndex()) {
+        this.shapeSliderIndex.set(newIndex);
+      }
+    });
+  }
+
+  updateShape(index: number): void {
+    this.shapeSliderIndex.set(index);
+    const selectedShape = this.shapes[index];
+    if (selectedShape) {
+      this.preferencesService.setShape(selectedShape.id);
+    }
+  }
 }
