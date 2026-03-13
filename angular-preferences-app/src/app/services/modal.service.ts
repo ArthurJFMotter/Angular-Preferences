fcad7fa042a739ed9b0bef9e5de0ee3f70c0d3bd
ArthurJFMotter <arthur.jf.motter@gmail.com>
2026-03-13 10:12:59 -0300

    feat(motion): modal fix

--- angular-preferences-app/src/app/services/modal.service.ts
+++ angular-preferences-app/src/app/services/modal.service.ts
@@ -1,21 +1,29 @@
 import { inject, Injectable } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 import { DynamicModalComponent } from '../components/dynamic-modal/dynamic-modal.component';
 import { ModalConfig } from '../models/modal.model';
+import { ThemeService } from './theme.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ModalService {
   private dialog = inject(MatDialog);
+  private themeService = inject(ThemeService);
 
   // --- CORE GENERIC METHOD ---
   openModal(config: ModalConfig) {
-    return this.dialog.open(DynamicModalComponent, {
+    const isReducedMotion = this.themeService.isReducedMotion();
+
+    const dialogConfig: MatDialogConfig = {
       data: config,
       autoFocus: 'first-tabbable',
-      panelClass: 'app-dynamic-modal-pane'
-    });
+      panelClass: 'app-dynamic-modal-pane',
+      enterAnimationDuration: isReducedMotion ? '0ms' : undefined,
+      exitAnimationDuration: isReducedMotion ? '0ms' : undefined,
+    };
+
+    return this.dialog.open(DynamicModalComponent, dialogConfig);
   }
 
   // --- PRE-CONFIGURED APP MODALS ---
@@ -23,15 +31,16 @@ export class ModalService {
     return this.openModal({
       title: 'Documentation',
       icon: 'menu_book',
-      message: 'The documentation is currently being written and will be available in a future update. Stay tuned!',
-      actions:[
+      message:
+        'The documentation is currently being written and will be available in a future update. Stay tuned!',
+      actions: [
         {
           label: 'Got it!',
           color: 'primary',
           isPrimary: true,
-          action: () => console.log('User acknowledged documentation status.')
-        }
-      ]
+          action: () => console.log('User acknowledged documentation status.'),
+        },
+      ],
     });
   }
 
@@ -39,15 +48,16 @@ export class ModalService {
     return this.openModal({
       title: 'MIT License',
       icon: 'gavel',
-      message: 'Copyright (c) 2026. Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files...',
-      actions:[
+      message:
+        'Copyright (c) 2026. Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files...',
+      actions: [
         {
           label: 'I Understand',
           color: 'primary',
           isPrimary: true,
-          action: () => console.log('License accepted')
-        }
-      ]
+          action: () => console.log('License accepted'),
+        },
+      ],
     });
   }
 }
