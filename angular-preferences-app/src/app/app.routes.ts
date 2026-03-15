11b8b65606f3abd33bae720f58d0af23751fa7ac
ArthurJFMotter <arthur.jf.motter@gmail.com>
2026-03-15 09:26:08 -0300

    feat(production): route fix

--- angular-preferences-app/src/app/app.routes.ts
+++ angular-preferences-app/src/app/app.routes.ts
@@ -1,19 +1,23 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component'; 
 
 export const routes: Routes =[
+    // --- Eagerly loaded routes ---
     {
         path: '',
-        loadComponent: () => import('./pages/home/home.component').then(c => c.HomeComponent),
+        component: HomeComponent, 
         pathMatch: 'full'
     },
     {
         path: 'home',
-        loadComponent: () => import('./pages/home/home.component').then(c => c.HomeComponent),
+        component: HomeComponent, 
     },
+    // --- Lazzy loaded routes ---
     {
         path: 'configurations',
         loadComponent: () => import('./pages/preferences/preferences.component').then(c => c.PreferencesComponent),
     },
+    // wildecard
     {
         path: '**',
         redirectTo: ''
