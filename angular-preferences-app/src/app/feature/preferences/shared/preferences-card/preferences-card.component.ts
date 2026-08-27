53d5e4ea34079c74a7eee6e26b891e30ad0c9fe1
ArthurJFMotter <arthur.jf.motter@gmail.com>
2026-08-27 08:58:17 -0300

    feat(config): preferences card update

--- angular-preferences-app/src/app/feature/preferences/shared/preferences-card/preferences-card.component.ts
+++ angular-preferences-app/src/app/feature/preferences/shared/preferences-card/preferences-card.component.ts
@@ -1,11 +1,11 @@
 import { Component, Input } from '@angular/core';
-import { MatCardModule } from '@angular/material/card';
+import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-preferences-card',
   standalone: true,
-  imports: [MatCardModule, MatIconModule],
+  imports: [CommonModule, MatIconModule],
   templateUrl: './preferences-card.component.html',
   styleUrl: './preferences-card.component.scss',
 })
